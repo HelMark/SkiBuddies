8631850a46decd4e64a324e0b7e760399a4fdbcd
2Gomaskin <2GoMaskin@2Go.no>
2023-07-14 19:17:18 +0200

    fixed some bugs

---
+++ backend/skibuddies_api/src/main/java/no/skibuddies/skibuddies_api/users/userController.java
@@ -0,0 +1,16 @@
+/*package no.skibuddies.skibuddies_api.users;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class userController {
+    
+    @GetMapping
+    public String allUsers() {
+        return "All users";
+    }
+}
+ */
