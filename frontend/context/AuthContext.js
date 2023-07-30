f6858848ab19072a4347d80860546fbcf11d3fff
sigurhag <sigurhag@ntnu.no>
2023-07-30 22:24:45 +0200

    updated Buddies

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -1,23 +1,67 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useState, useEffect} from "react";
+import AsyncStorge from "@react-native-async-storage/async-storage"
+import axios from "axios";
 
 export const AuthContext = createContext();
+const API_ENDPOINT  = "";
+
 
 export const AuthProvider = ({children}) => { 
     const [isLoading, setIsLoading] = useState(false);
     const [userToken, setUserToken] = useState(null);
+    const [userInfo, setUserInfo] = useState(null)
 
-    const login = () => {
+    const login = (username, password) => {
+        setIsLoading(true);
+        axios.post(API_ENDPOINT, {
+            username,
+            password
+        }).then(res => {
+            console.log(res.data)
+            let userInfo = res.data
+            setUserInfo(userInfo)
+            //setUserToken(userInfo.data.token)
+        }).catch(error => {
+            console.log("Login error: ", error)
+        });
         setUserToken("Random String");
+        //AsyncStorge.setItem("userToken", "Random String")
+        //AsyncStorge.setItem("userInfo", JSON.stringify(userInfo))
+        //AsyncStorge.setItem("userToken", userInfo.data.token)
         setIsLoading(false);
     }
     const logout = () => {
+        setIsLoading(true);
         setUserToken(null);
+        //AsyncStorge.removeItem("userToken")
+        //AsyncStorge.removeItem("userInfo")
         setIsLoading(false)
     }
 
+    const isLoggedIn = async() => {
+    //    try {
+    //       setIsLoading(true);
+    //        let userToken = await AsyncStorge.getItem("userToken");
+    //        let userInfo = await AsyncStorge.getItem("userInfo")
+    //        userInfo = JSON.parse("userInfo"):
+    //         if(userInfo) {
+    //           setUserToken("Random String") //Switch to userToken when ready
+    //           setUserInfo(userInfo)
+    //          }
+    //        setIsLoading(false)
+    //    } catch(error) {
+    //        console.error("isLogged error in: " , error)
+    //    }
+    }
+    useEffect(() => {
+        isLoggedIn();
+    }, []);
+
     return (
-    <AuthContext.Provider value={{login, logout, isLoading, userToken}}>
+    <AuthContext.Provider value={{login, logout, isLoading, userToken, userInfo}}> 
         {children}
     </AuthContext.Provider>
     )
 }   
+//Use userInfo to display info about the current logged in user in for example ProfileScreen
+
--- frontend/screens/Buddies.js
+++ frontend/screens/Buddies.js
@@ -1,74 +1,74 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions, SafeAreaView, Touchable, TouchableOpacity } from 'react-native';
 import SearchBar from "../components/SearchBar";
-import Swiper from "react-native-deck-swiper";
-import { MaterialIcons } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 
-const profiles = [
-  { id: 1, name: "profile 1", image: require("/Users/sigurdhagen/Documents/SkiBuddies/SkiBuddies/frontend/Bilder/bilde1.jpg") },
-  { id: 2, name: "profile 2", image: require("/Users/sigurdhagen/Documents/SkiBuddies/SkiBuddies/frontend/Bilder/bilde3.jpg") },
-  { id: 3, name: "profile 3", image: require("/Users/sigurdhagen/Documents/SkiBuddies/SkiBuddies/frontend/Bilder/bilde4.jpeg") }
-];
-
-const { width } = Dimensions.get('window');
+const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
+
+const API_ENDPOINT = ""; //add API endpoint
+
+const profiles = [ // Dummie values. Switch with: const [profiles, setProfiles] = useState([]);
+  { id: 1, name: "Bob", age: 23, source: require("../Bilder/bilde1.jpg") },
+  { id: 2, name: "John", age: 55, source: require("../Bilder/bilde3.jpg") },
+  { id: 3, name: "Mike", age: 69, source: require("../Bilder/bilde4.jpeg") },
+  { id: 4, name: "Karen", age: 42, source: require("../Bilder/bilde5.jpeg") },
+  { id: 5, name: "Kim", age: 29, source: require("../Bilder/bilde6.jpeg") },
+  { id: 6, name: "Siri", age: 21, source: require("../Bilder/bilde7.jpeg") }
+];
 
 export default function Buddies() {
   const [fontLoaded, setFontLoaded] = useState(false);
-    const loadFonts = async () => {
-        await Font.loadAsync({
-          'Roboto-Regular': require('../fonts/Roboto-Regular.ttf'),
-        });
-        setFontLoaded(true)
-      };
-      useEffect(() => {
-        loadFonts(); 
-      }, []);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'Roboto-Regular': require('../fonts/Roboto-Regular.ttf'),
+    });
+    setFontLoaded(true);
+  };
+
+  const fetchProfiles = async () => {
+    try {
+        const reponse = await axios.get("") //Path to Profiles
+        setProfiles(reponse.data);
+    } catch {
+        console.error("Error fetching images:", error);
+    }
+}
+
+  useEffect(() => {
+    loadFonts();
+    //fetchProfiles();
+  }, []);
 
   const handleSearch = (searchText) => {
-    //Logic for search for profile
-    console.log(searchText)
- };
-
-    const renderCard = (profile) => {
-    return (
-    <View style={styles.card}>
-      <View style={styles.imageContainer}>
-        <Image source={profile.image} style={styles.image} />
-        {fontLoaded &&<Text style={styles.profileId}>{profile.id}</Text>}
-       {fontLoaded &&<Text style={styles.profileName}>{profile.name}</Text>}
-      </View>
-
-    </View>
-     );
-    };
+    // Logic for search for profile
+    console.log(searchText);
+  };
+  const handleProfilePress = () => {
+    //Logic for showing profile
+  }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
-        <SearchBar onSearch={handleSearch} placeholder={"Search for profiles..."} />
-        {fontLoaded &&<Text style={styles.headerText}>Recommended Profiles:</Text>}
+        <SearchBar onSearch={handleSearch} placeholder="Search for a profile..." />
+        {fontLoaded && <Text style={styles.headerText}>Recommended Profiles</Text>}
       </View>
-      <View style={styles.contentContainer}>
-        <Swiper
-          cards={profiles}
-          renderCard={renderCard}
-          onSwipedLeft={(index) => console.log("swiped left")}
-          onSwipedRight={(index) => console.log("swiped right")}
-          cardStyle={styles.swiperCard}
-          disableBottomSwipe={true}
-          disableTopSwipe={true}
-          backgroundColor="white"
-          cardVerticalMargin={10}
-          stackSize={3} //Might need to change this 
-        />
+      <View style={styles.gridContainer}>
+        {profiles.map((profile) => (
+          <View key={profile.id} style={styles.profileContainer}>
+            <TouchableOpacity onPress={handleProfilePress}>
+            <Image source={profile.source} style={styles.image} />
+            <View style={styles.profileInfo}>
+              <Text style={styles.name}>{profile.name}</Text>
+              <Text style={styles.age}>{profile.age} years old</Text>
+            </View>
+            </TouchableOpacity>
+          </View>
+        ))}
       </View>
-      <View style={styles.bottomIconContainer}>
-        <MaterialIcons name="highlight-remove" size={52} color="#E54C38" style={styles.icon} />
-        <MaterialIcons name="add-circle-outline" size={52} color="#00AB41" style={styles.icon} />
-        </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -78,90 +78,47 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   headerContainer: {
-    alignItems: "center"
+    alignItems: "center",
+    borderBottomColor: "#0096FF",
+    borderBottomWidth: 2
   },
   headerText: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#0096FF",
     marginTop: 10,
+    marginBottom: 4,
     fontFamily: "Roboto-Regular"
   },
-  contentContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "white",
+  gridContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
-  swiperCard: {
-    width: width-40,
-    height: screenHeight*0.62,
-    borderRadius: 10,
-    backgroundColor: 'white',
-    elevation: 4,
-    shadowColor: 'rgba(0, 0, 0, 0.2)',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    overflow: 'hidden'
-  },
-  imageContainer: {
-    position: 'relative',
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
+  profileContainer: {
+    margin: 5,
+    alignItems: "center",
   },
   image: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+    width: screenWidth / 2.3,
+    height: screenHeight / 4.6,
     borderRadius: 10,
   },
-  profileId: {
-    position: 'absolute',
-    top: 10,
-    left: 10,
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-    paddingHorizontal: 10,
+  profileInfo: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    borderRadius: 10,
     paddingVertical: 5,
-    borderRadius: 5,
-    color: 'white',
+    paddingHorizontal: 10,
+  },
+  name: {
+    color: "white",
     fontSize: 16,
-    fontWeight: 'bold',
-    fontFamily: "Roboto-Regular"
+    fontWeight: "bold",
   },
-  profileName: {
-    position: 'absolute',
-    top: "80%",
-    right: "65%",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 5,
-    color: 'white',
-    fontSize: 26,
-    fontWeight: 'bold',
-    fontFamily: "Roboto-Regular"
-  },
-  bottomIconContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-end",
-    top: screenHeight*0.635
-  },
-  icon: {
-    zIndex: 1,
-    marginHorizontal: 25,
-    shadowColor: "#000",
-        shadowOffset: {
-        width: 0,
-        height: 2,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 2,
-        elevation: 2
-  },
+  age: {
+    color: "white",
+    fontSize: 12,
+  }
 });
-
